f2bd33c2f223309735edc1673cf81a86d10dfbc9
Formbondhu <meta500681@gmail.com>
2025-05-28 11:02:27 +0600

    Update script.js
    Signed-off-by: Formbondhu <meta500681@gmail.com>

--- script.js
+++ script.js
@@ -144,16 +144,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const { jsPDF } = window.jspdf;
 
     // Firebase Initialization
-    const firebaseConfig = {
-        apiKey: "AIzaSyCoIdMx9Zd7kQt9MSZmowbphaQVRl8D16E",
-        authDomain: "admissionformdb.firebaseapp.com",
-        projectId: "admissionformdb",
-        storageBucket: "admissionformdb.firebasestorage.app",
-        messagingSenderId: "398052082157",
-        appId: "1:398052082157:web:0bc02d66cbdf55dd2567e4",
-    };
+const firebaseConfig = {
+    apiKey: "AIzaSyCoIdMx9Zd7kQt9MSZmowbphaQVRl8D16E",
+    authDomain: "admissionformdb.firebaseapp.com",
+    projectId: "admissionformdb",
+    storageBucket: "admissionformdb.firebasestorage.app",
+    messagingSenderId: "398052082157",
+    appId: "1:398052082157:web:0bc02d66cbdf55dd2567e4",
+};
+
+// Firebase ইনিশিয়ালাইজ করা (শুধু একবার)
+if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
-    const db = firebase.firestore();
+}
+// গ্লোবাল স্কোপে db রাখা
+window.db = firebase.firestore();
 
     // Navigation Events
     const homeIcon = document.querySelector('.home-icon');
